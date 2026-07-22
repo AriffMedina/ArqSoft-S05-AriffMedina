03664bf228948fc242d4df1c6f6cc574926bfb74
AriffMedina <ariffmedina.contact@gmail.com>
2026-07-21 20:56:09 -0600

    Test que falla

---
+++ CitasApp.xUnit/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using CitasApp.Application.Services;
+using Xunit;
+
+namespace CitasApp.xUnit
+{
+    public class CalculadoraServiceTests
+    {
+        [Fact]
+        public void Dividir_PorCero_LanzaExcepcion()
+        {
+            var calc = new CalculadoraService();
+            Assert.Throws<DivideByZeroException>(() => calc.Dividir(10, 0));
+        }
+    }
+}
